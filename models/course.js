addbc61ef9c836b1b45510abf6ce441c49d8a115
lh-25 <sprinklecake25@gmail.com>
2024-10-20 15:06:07 -0500

    created login/signup forms

--- models/course.js
+++ models/course.js
@@ -3,7 +3,13 @@ const { Schema } = require('mongoose')
 
 const Course = new Schema(
     {
-       
+       name: {type: String, required: true},
+       schedule: {type: String, enum: ['mon','tues','wed','thurs','fri'], required: true},
+       description: {type: String, required: true},
+       courseCode: {type: Number, required: true}, 
+       seatsAvalible: {type: Number, required: true},
+       teacher: {type: mongoose.Schema.Types.ObjectId, ref: 'Teacher'},
+       students:[ {type: mongoose.Schema.Types.ObjectId, ref: 'Teacher'}]
     },
     {timestamps: true},
 )
